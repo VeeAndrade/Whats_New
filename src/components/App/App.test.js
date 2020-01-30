0137613dfb510b27441e40e6156e8df1993d7b07
Veronica Andrade <veronica726@gmail.com>
2020-01-30 16:30:17 -0700

    Add App Test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import local from '../../data/local';
+import health from '../../data/health'
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('should have a state', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.state('local')).toEqual(local)
+  });
+
+  it('should show search results', () => {
+    const wrapper = shallow(<App />);
+
+    wrapper.instance().showSearchResults(local)
+    expect(wrapper.state('local')).toEqual(local)
+  });
+
+  it('should change the the current page', () => {
+    const wrapper = shallow(<App />);
+    const catagory = 'health';
+
+    wrapper.instance().changeCurrentPage(catagory)
+    expect(wrapper.state(catagory)).toEqual(health);
+  });
 });
