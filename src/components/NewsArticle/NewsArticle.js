fbdb27425628f0adb98181dc6f25283041059d0f
Veronica Andrade <veronica726@gmail.com>
2020-01-30 16:35:30 -0700

    News Article functional components

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import './NewsArticle.css';
 
-// NEWSARTICLE COMPONENT CODE GOES HERE
+const NewsArticle = ({headline, image, url, description}) => {
+  return <section className='article-section'>
+    <img className='article-image' src={image} alt='news'></img>
+    <section className='article-info'>
+      <h2 className='article-headline'>{headline}</h2>
+      <p className='article-description'>{description}</p>
+    </section>
+    <section className='url-section'>
+      <a className='article-url' href={url}>Link To article</a>
+      <p className='article-url-arrow'>→</p>
+    </section>
+  </section>
+}
 
 export default NewsArticle;
