302cbdd16a361ffa0c9243db4f0f48219d193529
Veronica Andrade <veronica726@gmail.com>
2020-01-30 16:33:33 -0700

    Add menu functional components

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import './Menu.css'
 
-// MENU COMPONENT CODE GOES HERE
+const Menu = (props) => {
+  const grabName = (event) => {
+    props.changeCurrentPage(event.target.name)
+  }
+
+  return (
+    <section className='menu-section'>
+      <button name='local' className='menu-btn' onClick={grabName}>❖ Local</button>
+      <button name='entertainment' className='menu-btn' onClick={grabName}>❖ Entertainment</button>
+      <button name='health' className='menu-btn' onClick={grabName}>❖ Health</button>
+      <button name='science' className='menu-btn' onClick={grabName}>❖ Science</button>
+      <button name='technology' className='menu-btn' onClick={grabName}>❖ Technology</button>
+    </section>
+  )
+}
 
 export default Menu;
